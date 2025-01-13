c5bd45403a7e063245fd520679e8170446a9d798
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:49:40 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,58 +1,54 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState('abc');
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  // Check if there's a user in localStorage
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      axios
-        .get('http://localhost:5000/api/auth/me', {
-          headers: { Authorization: `Bearer ${token}` },
-        })
-        .then((res) => setUser(res.data))
-        .catch((err) => {
-          localStorage.removeItem('token');
-          setUser(null);
-        });
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (token) {
+          const { data } = await axios.get(`http://localhost:3010/api/auth/me`, {
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          setUser(data);
+        }
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (!user) {
+      fetchUser();
     }
+
   }, []);
 
-  // Login function
   const login = async (email, password) => {
-    try {
-      const { data } = await axios.post(
-        'http://localhost:5000/api/auth/login',
-        { email, password }
-      );
-      localStorage.setItem('token', data.token);
-      setUser(
-        await axios.get('http://localhost:5000/api/auth/me', {
-          headers: { Authorization: `Bearer ${data.token}` },
-        })
-      );
-    } catch (err) {
-      console.log(err);
-    }
+  const { data } =   await axios.post(`http://localhost:3010/api/auth/login`, { email, password });
+    localStorage.setItem('token', data.token);
+    setUser(data.user);
   };
 
-  // Logout function
   const logout = () => {
     localStorage.removeItem('token');
     setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, loading, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
 export const useAuth = () => {
-  return React.useContext(AuthContext);
+  return useContext(AuthContext);
 };
