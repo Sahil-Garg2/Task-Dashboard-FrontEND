3a806c9e33a815e4679b9dcf5abae764efefd4e4
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:49:21 +0530

    Create SignupPage.jsx

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Input, Ripple, initMDB } from "mdb-ui-kit";
+import { ModalDialog } from '../components/Modal';
+
+
+const SignupPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [role, setRole] = useState('677e9880cc9acd6d9be5a53a');
+  const modalRef = React.createRef();
+  useEffect(() => { 
+
+    initMDB({ Input, Ripple });
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post(`http://localhost:3010/api/auth/signup`, {
+        email,
+        password,
+        username,
+        role,
+      });
+      modalRef.current.show('Success', 'User created successfully');
+    } catch (error) {
+      console.error('Error creating user:', error);
+      modalRef.current.show('Error', 'Error creating user');  
+    }
+  };
+
+  return (
+    <div>
+      <section className="vh-100" style={{ backgroundColor: "#eee"}}>
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-12 col-xl-11">
+              <div className="card text-black" style={{ "borderRadius":"25px" }}>
+                <div className="card-body p-md-5">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                      <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                            <input
+                              type="text"
+                              placeholder="Username"
+                              value={username}
+                              onChange={(e) => setUsername(e.target.value)}
+                              id="form3Example1c" className="form-control"
+                            />
+                            <label className="form-label" htmlFor="form3Example1c">User Name</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                          <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                            <input
+                              type="email"
+                              placeholder="Email"
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                              id="form3Example3c" className="form-control"
+                            />
+                            <label className="form-label" htmlFor="form3Example3c">Your Email</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              placeholder="Password"
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                              id="form3Example4c"
+                              className="form-control"
+                            />
+                            <label className="form-label" htmlFor="form3Example4c">Password</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                          <div data-mdb-input-init className="form-outline flex-fill mb-0">
+                            <input
+                              type="text"
+                              placeholder="Role"
+                              value={role}
+                              onChange={(e) => setRole(e.target.value)}
+                              id="form3Example4cd" className="form-control"
+                            />
+                            <label className="form-label" htmlFor="form3Example4cd">Role</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-lg">Register</button>
+                        </div>
+
+                      </form>
+
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                        className="img-fluid" alt="Sample image"></img>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <ModalDialog ref={modalRef}/>
+    </div>
+
+    
+  );
+};
+
+export default SignupPage;
