ba23070cdf2c6ea3aeefd56b9622b6fcdad0b659
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:48:52 +0530

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import { TaskProvider } from '../context/TaskContext';
+import Sidebar from '../components/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import TaskList from '../components/TaskList';
+import { UserProvider } from '../context/UserContext';
 
 const DashboardPage = () => {
+  const { user } = useAuth();
+
+  if (user && user.role.name !== 'user') {
+    return <h1>You are not authorized to view this page.</h1>;
+  }
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to your dashboard!</p>
-    </div>
+    <>
+      <div className="row">
+        <div className="col-3">
+          <Sidebar></Sidebar>
+        </div>
+        <div className="col-9">
+          {/* Task Data */}
+          <TaskProvider>
+            <UserProvider>
+              <Routes>
+              <Route path="tasks" element={<TaskList isUser={ true} />} />
+              </Routes>
+            </UserProvider>
+          </TaskProvider>
+        </div>
+      </div>
+    </>
   );
 };
 
