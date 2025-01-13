cf58cc9495eb9a2f5d7d62553263ac955122c7e6
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:50:06 +0530

    Create CategoryContext.jsx

---
+++ src/context/CategoryContext.jsx
@@ -0,0 +1,53 @@
+// context/CategoryContext.js
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+const CategoryContext = createContext();
+
+export const CategoryProvider = ({ children }) => {
+    const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // Fetch all categories (Admin view)
+    const fetchCategories = async () => {
+        try {
+            const response = await axios.get('http://localhost:3010/api/categories');
+            setCategories(response.data);
+            setLoading(false);
+        } catch (err) {
+            console.error('Error fetching categories', err);
+        }
+    };
+
+    // Create a new category
+    const createCategory = async (categoryData) => {
+        try {
+            const response = await axios.post('http://localhost:3010/api/categories/create', categoryData, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            });
+
+            setCategories([...categories, response.data]);
+            return true;
+        } catch (err) {
+            //modalRef.current.show('Error', `Error creating category: ${err.message}`);
+            console.error('Error creating category', err);
+            return false;
+        }
+    };
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    return (
+        <CategoryContext.Provider value={{ categories, createCategory }}>
+            {children}
+        </CategoryContext.Provider>
+    );
+};
+
+export const useCategory = () => {
+    return React.useContext(CategoryContext);
+};
