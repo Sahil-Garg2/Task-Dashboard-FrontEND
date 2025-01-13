80f6eadc378a1c749c5e028d55ed18c82fe34443
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:52:55 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+
+function Navbar() {
+    const { user, logout } = useAuth();
+
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+            <div className="container-fluid">
+                <button
+                    data-mdb-collapse-init
+                    className="navbar-toggler"
+                    type="button"
+                    data-mdb-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <i className="fas fa-bars"></i>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <a className="navbar-brand mt-2 mt-lg-0" href="#">
+                        <img
+                            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+                            height="15"
+                            alt="MDB Logo"
+                            loading="lazy"
+                        />
+                    </a>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        {/* make it conditional */}
+                        {!user && (
+                            <><li className="nav-item">
+                            <a className="nav-link" href="/login">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/signup">Signup</a>
+                        </li></>
+                        )}
+                        
+                        
+                    </ul>
+                </div>
+                <div className="d-flex align-items-center">
+                    { 
+                        user && (
+                            <button onClick={logout}>logout</button>
+                        )
+                    }
+                </div>
+            </div>
+
+        </nav>
+    );
+}
+
+export default Navbar;
