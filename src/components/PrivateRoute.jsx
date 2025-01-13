8d2cc896a21d24fc6dc18c73d37451297ef3cd25
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:53:17 +0530

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,10 +3,26 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { user ,loading, logout, login} = useAuth();
 
+  if (loading) {
+    return <div>loading</div>;
+  }
   if (!user) {
     return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export const LoginRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>loading</div>;
+  }
+  if (user) {
+    return <Navigate to="/admin" />;
   }
 
   return children;
