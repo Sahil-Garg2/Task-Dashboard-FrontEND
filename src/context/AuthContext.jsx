a1bebb6c7e402c87e67f9fb741727e9acf705eb6
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-14 21:41:28 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import { Navigate, useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext();
 
